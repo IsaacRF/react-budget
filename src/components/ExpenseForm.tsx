d97477d977b93eba2878c0d7a391fec300a535e4
Isaac R.F <isaacrf239@gmail.com>
2021-04-23 09:44:38 +0200

    #77-78 Add states, validation, error messages and bind fields to their values

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,11 +1,35 @@
-import React, { useState } from "react";
+import { stringify } from "node:querystring";
+import React, { FormEvent, useState } from "react";
+import Error from "./Error";
 
 export interface ExpenseFormProps {}
 
 const ExpenseForm: React.FC<ExpenseFormProps> = () => {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [error, setError] = useState(false);
+
+  const addExpense = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(amount);
+    console.log(name);
+
+    //Validation
+    if (amount < 1 || name === "") {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+  };
+
   return (
-    <form>
+    <form onSubmit={addExpense}>
       <h2>Add Expense</h2>
+
+      {error &&
+        <Error message="Amount or name not set" />
+      }
 
       <div className="field">
         <label>Name</label>
@@ -13,6 +37,8 @@ const ExpenseForm: React.FC<ExpenseFormProps> = () => {
           type="text"
           className="u-full-width"
           placeholder="e.g. Restaurant Lunch"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
 
@@ -22,6 +48,8 @@ const ExpenseForm: React.FC<ExpenseFormProps> = () => {
           type="number"
           className="u-full-width"
           placeholder="e.g. 300"
+          value={amount}
+          onChange={(e) => setAmount(parseInt(e.target.value, 10) || 0)}
         />
       </div>
 
@@ -29,7 +57,7 @@ const ExpenseForm: React.FC<ExpenseFormProps> = () => {
         type="submit"
         className="button-primary u-full-width"
         value="Add Expense"
-        />
+      />
     </form>
   );
 };
